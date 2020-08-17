8bfe0c835aac4506857e21afddeb663b8c83a42f
GitHub Action <action@github.com>
2020-08-17 17:48:09 +0000

    Added Build Changes

--- lib/interfaces/modelManifestItem.d.ts
+++ lib/interfaces/modelManifestItem.d.ts
@@ -2,6 +2,7 @@ import { StudyType } from "../enums/studyTypes.enum";
 import { Modality } from "../enums/modality.enum";
 import { ModelInputs } from "../enums/modelInputs.enum";
 import { ModelOutputs } from "../enums/modelOutputs.enum";
+import { DocParameter } from "./documentation";
 export interface ModelManifestItem {
     tag: string;
     displayName: string;
@@ -11,3 +12,4 @@ export interface ModelManifestItem {
     output: ModelOutputs;
     hasImageOutput: boolean;
 }
+export declare const docParams: Array<DocParameter>;
