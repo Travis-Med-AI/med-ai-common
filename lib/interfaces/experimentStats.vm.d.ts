9aa19f75b0abc1fab25f84c08cf25ea790532518
GitHub Action <action@github.com>
2021-10-17 03:24:57 +0000

    Added Build Changes

---
+++ lib/interfaces/experimentStats.vm.d.ts
@@ -0,0 +1,16 @@
+export interface ExperimentStats {
+    total: number;
+    accuracy: number;
+    fpr: number;
+    tpr: number;
+    sensitivity: number;
+    specificity: number;
+    ppv: number;
+    npv: number;
+}
+export interface ExperimentStatusViewModel {
+    experimentId: number;
+    keys: {
+        [key: string]: ExperimentStats;
+    };
+}
